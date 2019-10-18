7f43191210c419df3824142d4e92b2c55f516c38
SebaDominguez <s.domnguezferrero@uandresbello.edu>
2019-10-18 06:21:48 -0300

    Prescripcion form y metodo http post
    Se creo el formulario de la prescripción y tambien se añadió el metodo http post

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit{
 
-  constructor() {}
+  formulario: FormGroup;
 
-}
+  profesional: Array<string>;
+
+  constructor(private http: HttpClient, public formBuilder: FormBuilder, private router: Router) {}
+
+  ngOnInit () {
+
+    this.profesional = [
+      "Psicólogo",
+      "Nutricionista",
+      "Enfermera",
+      "Kinesiólogo"
+    ];
+
+    this.formulario = this.formBuilder.group({
+
+      profesional: new FormControl (this.profesional[0]),
+      nombre: new FormControl (''),
+      apellido: new FormControl (''),
+      rut: new FormControl (''),
+      observacion: new FormControl('')
+    });
+
+  }
+
+    postData(){
+      const value = this.formulario.value;
+      var url= 'http://localhost:3000/databases.json';
+      this.http.post(url, {
+        profesional: value.profesional,
+        nombre: value.nombre,
+        apellido: value.apellido,
+        rut: value.rut,
+        observacion: value.observacion,
+      })
+      .subscribe((response) => {
+      console.log(response);
+      });
+    };
+
+  }
+
+
+
